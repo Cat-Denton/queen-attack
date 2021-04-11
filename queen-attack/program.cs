3dad3b81ccdb01c774f9fdd938ff25bbce60fb69
Cat-Denton <willwdenton@gmail.com>
2021-04-11 16:54:31 -0700

    finish writing console logs to test queen constructor
    Co-authored-by: Tyler Sinks <tyler.sinks@gmail.com>

--- queen-attack/program.cs
+++ queen-attack/program.cs
@@ -11,6 +11,8 @@ namespace Chess
       int[] whiteQueenPos = {4,1};
       Queen queen = new Queen(whiteQueenPos, false, "white");
       Console.WriteLine("the " + queen.Color + " Queen has entered the playing field");
+      Console.WriteLine(Convert.ToString(queen.Position[0]) + ", " + Convert.ToString(queen.Position[1]) + " are the current coordinates of the " + queen.Color + " queen");
+      Console.WriteLine("This piece has been captured? " + Convert.ToString(queen.Captured));
     }
   }
 }
